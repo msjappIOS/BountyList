46e4a2c7a8f23c4624d7abdc54e469f78e135184
SeokJinMoon <damul1992@gmail.com>
2021-03-22 16:00:06 +0900

    CollectionView 적용

--- BountyList/BountyViewController.swift
+++ BountyList/BountyViewController.swift
@@ -7,7 +7,49 @@
 
 import UIKit
 
-class BountyViewController: UIViewController , UITableViewDataSource, UITableViewDelegate {
+class BountyViewController: UIViewController , UICollectionViewDataSource, UICollectionViewDelegate, UICollectionViewDelegateFlowLayout {
+    
+    
+    
+    //UICollectionViewDataSource
+    //몇개를 보여줄 것인지..?
+    //셀은 어떻게 표현할 것인지?
+
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return viewModel.numOfBountyInfoList
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridCell", for: indexPath) as? GridCell else {
+            return UICollectionViewCell()
+        }
+        
+        if let info = viewModel.bountyInfo(indexPath.item){
+            cell.update(val : info)
+        }
+        return cell
+    }
+
+    //UICollectionViewDelegate
+    //셀이 클릭되었을 때 어떻게 할 것인지?
+    
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+        self.performSegue(withIdentifier: "showDetail", sender: indexPath.item)
+        
+    }
+
+    //UICollectionViewDelegateFlowLayout
+    //cell size 계산 ( 목표 : 다양한 디바이스에서 일관적인 디자인을 보여주기 위해 )
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+            let itemSpacing: CGFloat = 10 // 가로에서 cell과 cell 사이의 거리
+            let textAreaHeight: CGFloat = 65 // textLabel이 차지하는 높이
+            let width: CGFloat = (collectionView.bounds.width - itemSpacing)/2 // 셀 하나의 너비
+            let height: CGFloat = width * 10/7 + textAreaHeight //셀 하나의 높이
+
+            return CGSize(width: width, height: height)
+        }
+
     
     //MVVM
     
@@ -42,29 +84,29 @@ class BountyViewController: UIViewController , UITableViewDataSource, UITableVie
             }
         }
     }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return viewModel.numOfBountyInfoList
-    }
-    
-    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
-            return UITableViewCell()
-        }
-        
-        if let info = viewModel.bountyInfo(indexPath.row) {
-            cell.update(val : info)
-        }
-        
-        return cell
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        
-        print("--> \(indexPath.row)")
-        self.performSegue(withIdentifier: "showDetail", sender: indexPath.row)
-    }
-    
+//
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return viewModel.numOfBountyInfoList
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? ListCell else {
+//            return UITableViewCell()
+//        }
+//
+//        if let info = viewModel.bountyInfo(indexPath.row) {
+//            cell.update(val : info)
+//        }
+//
+//        return cell
+//    }
+//
+//    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+//
+//        print("--> \(indexPath.row)")
+//        self.performSegue(withIdentifier: "showDetail", sender: indexPath.row)
+//    }
+//
     func setBountyInfo() {
         
         let nameList = viewModel.getNameList()
@@ -77,13 +119,25 @@ class BountyViewController: UIViewController , UITableViewDataSource, UITableVie
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        tableView.delegate = self
-        tableView.dataSource = self
+//        tableView.delegate = self
+//        tableView.dataSource = self
         setBountyInfo()
     }
 }
 
 class ListCell : UITableViewCell {
+    @IBOutlet weak var imgView : UIImageView!
+    @IBOutlet weak var nameLabel : UILabel!
+    @IBOutlet weak var bountyLabel : UILabel!
+    
+    func update(val : BountyInfo) {
+        imgView.image = val.image
+        nameLabel.text = val.name
+        bountyLabel.text = "$\(val.bounty)"
+    }
+}
+
+class GridCell : UICollectionViewCell {
     @IBOutlet weak var imgView : UIImageView!
     @IBOutlet weak var nameLabel : UILabel!
     @IBOutlet weak var bountyLabel : UILabel!
